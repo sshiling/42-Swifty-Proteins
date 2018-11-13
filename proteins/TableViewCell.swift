aa1dc58ffee2b5ba8091cc9e674b000c95d2738c
oskul <olgaskulska@gmail.com>
2018-11-13 16:51:33 +0200

    adding infromation to a view

---
+++ proteins/TableViewCell.swift
@@ -0,0 +1,26 @@
+//
+//  TableViewCell.swift
+//  proteins
+//
+//  Created by Olga SKULSKA on 11/13/18.
+//  Copyright © 2018 Sergiy SHILINGOV. All rights reserved.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+
+    @IBOutlet weak var ligoldName: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
