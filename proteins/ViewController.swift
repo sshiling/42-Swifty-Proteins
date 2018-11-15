0ebd4f3bea50a5978c36d123abc32f9c3c3e80e7
oskul <olgaskulska@gmail.com>
2018-11-15 20:39:58 +0200

    fix btn

--- proteins/ViewController.swift
+++ proteins/ViewController.swift
@@ -10,15 +10,35 @@ import UIKit
 import LocalAuthentication
 
 class ViewController: UIViewController {
+
+    @IBOutlet weak var loginView: UITextField!
+    @IBOutlet weak var passwordView: UITextField!
+    @IBOutlet weak var button: UIButton!
     
     let context = LAContext()
     var error: NSError?
     var proteinsArr: [String] = []
     
-    @IBOutlet weak var loginView: UITextField!
-    @IBOutlet weak var passwordView: UITextField!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        button.isHidden = true
+        if context.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &error) {
+            button.isHidden = false
+        }
+        
+        let fileURLProject = Bundle.main.path(forResource: "ligands", ofType: "txt")
+        var readStringProject = ""
+        do {
+            readStringProject = try String(contentsOfFile: fileURLProject!, encoding: String.Encoding.utf8)
+        } catch let error as NSError {
+            print("Failed reading from URL: \(String(describing: fileURLProject)), Error: " + error.localizedDescription)
+        }
+        proteinsArr = readStringProject.components(separatedBy: "\n").filter({!$0.isEmpty})
+    }
     
     @IBAction func loginButton(_ sender: Any) {
+        
         loginView.text = ""
         passwordView.text = ""
         DispatchQueue.main.async {
@@ -26,13 +46,12 @@ class ViewController: UIViewController {
         }
     }
     
-    @IBOutlet weak var button: UIButton!
-    
     @IBAction func authWithTouchID(_ sender: Any) {
-        self.button.isEnabled = false
-        let reason = "Authenticate with Touch ID"
         loginView.text = ""
         passwordView.text = ""
+        self.button.isEnabled = false
+        
+        let reason = "Authenticate with Touch ID"
         loginView.resignFirstResponder()
         passwordView.resignFirstResponder()
         context.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, localizedReason: reason, reply:
@@ -42,13 +61,12 @@ class ViewController: UIViewController {
                         self.performSegue(withIdentifier: "goToTableView", sender: self)
                         self.button.isEnabled = true
                     }
-                    
                 }
                 else {
                     self.showAlertController("Touch ID Authentication Failed")
                     self.button.isEnabled = true
                 }
-            })
+        })
     }
     
     func showAlertController(_ message: String) {
@@ -57,34 +75,10 @@ class ViewController: UIViewController {
         present(alertController, animated: true, completion: nil)
     }
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        button.isHidden = true
-    
-        // check if Touch ID is available
-        if context.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &error) {
-            button.isHidden = false
-        }
-        
-        let fileURLProject = Bundle.main.path(forResource: "ligands", ofType: "txt")
-        var readStringProject = ""
-        
-        do {
-            readStringProject = try String(contentsOfFile: fileURLProject!, encoding: String.Encoding.utf8)
-        } catch let error as NSError {
-            print("Failed reading from URL: \(String(describing: fileURLProject)), Error: " + error.localizedDescription)
-        }
-        proteinsArr = readStringProject.components(separatedBy: "\n").filter({!$0.isEmpty})
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-    }
-
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if segue.identifier == "goToTableView" {
-            let destinationVC = segue.destination as! TableViewController
             
+            let destinationVC = segue.destination as! TableViewController
             destinationVC.names = proteinsArr
         }
     }
